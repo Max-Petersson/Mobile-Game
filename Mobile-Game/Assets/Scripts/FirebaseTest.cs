55bac41577654020497e2783b6cdf344df7f7572
Risalord <max.hej777@gmail.com>
2023-01-18 16:46:28 +0100

    Fixed a lobby system

--- Mobile-Game/Assets/Scripts/FirebaseTest.cs
+++ Mobile-Game/Assets/Scripts/FirebaseTest.cs
@@ -1,17 +1,49 @@
 using UnityEngine;
-
 using Firebase;
 using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Extensions;
+using System.Collections.Generic;
 
+public class GameSessions
+{
+    public string path = "";
+    public string spot1 = "";
+    public string spot2 = "";
+    public GameSessions(string path, string spot1, string spot2)
+    {
+        this.path = path;
+        this.spot1 = spot1;
+        this.spot2 = spot2;
+    }
+    
+}
+public class PlayerProp
+{
+    string id;
+    string name;
+
+    public PlayerProp(string id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
+public class Game
+{
+    public string Player1 = "none";
+    public string Player2 = "none";
+    
+}
 public class FirebaseTest : MonoBehaviour
 {
     private const string GAMELOBBY = "GameLobby";
     private const string GAMESESSION = "Gamesession";
     FirebaseDatabase db;
     FirebaseAuth auth;
-
+    FirebaseApp app;
+    DatabaseReference dbRef;
+    private string myPath = "";
     void Start()
     {
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
@@ -21,7 +53,7 @@ public class FirebaseTest : MonoBehaviour
 
             auth = FirebaseAuth.DefaultInstance;
             db = FirebaseDatabase.DefaultInstance;
-            ListenForPlayerThrow();
+            app = FirebaseApp.Create();
         });
         
     }
@@ -97,6 +129,7 @@ public class FirebaseTest : MonoBehaviour
             if (task.Exception != null)
                 Debug.Log(task.Exception);
         });
+        ListenForPlayerThrow();
     }
     private void GetInfo() // this should read the data of the other player throw and put it into a method //this writes to the database
     {
@@ -117,8 +150,7 @@ public class FirebaseTest : MonoBehaviour
     }
     private void TryCreateGame() // This should be done when player presses start game
     {
-
-        db.GetReference(GAMELOBBY).LimitToFirst(1).GetValueAsync().ContinueWithOnMainThread(task => //here we have a way of going thorugh each game session just replace the 1 with an int
+        db?.GetReference(GAMELOBBY).Child(GAMESESSION).GetValueAsync().ContinueWithOnMainThread(task => //here we have a way of going thorugh each game session just replace the 1 with an int
         {
             if(task.Exception != null)
             {
@@ -127,32 +159,105 @@ public class FirebaseTest : MonoBehaviour
             else 
             {
                 DataSnapshot snapshot = task.Result;
-                Debug.Log(snapshot.GetRawJsonValue());
-                string game = snapshot.GetRawJsonValue();
-                string bas = game.Remove(0,24);
-                //bas = bas.Remove(bas.Length, 1);
-                string res = bas.Remove(bas.Length-1, 1);
-                Debug.Log(res);
-                AimMessage ai = JsonUtility.FromJson<AimMessage>(res);
-                Debug.Log(ai.Gamesession);
+
+               
+                string blo = snapshot.GetRawJsonValue();
+
+                string theObject = blo.Replace("Player1", "").Replace("Player2", "");
+               
+                char[] delimitercharacters = {':', ','};
+                string[] keysAndValues = theObject.Split(delimitercharacters);
+
+                List<string> keys = new List<string>();
+
+                foreach (string key in keysAndValues)
+                {
+                    string blue = key.Trim(new char[] { '{', '}', '"' });
+                    if(blue.Length > 3)
+                    {
+                        keys.Add(blue);
+                    }
+                       
+
+                }
+                int keeper = 1;
+                string spot1 = "";
+                string spot2 = "";
+                string path = "";
+
+                Debug.Log(keys.Count);
+
+                List <GameSessions> gameSessions = new List <GameSessions>();
+                for(int i = 0; i < keys.Count; i++)
+                {
+                    if(i == 0 || i % 3 == 0)
+                    {
+                        path = keys[i];
+                        //its a key
+                    }
+                    else if(i == 1 || i % 3 == 1)
+                    {
+                        spot1 = keys[i];
+                        //spot 2
+                    }
+                    else if(i == 2 || i % 3 == 2)
+                    {
+                        spot2 = keys[i];
+                        GameSessions gamesesh = new(path, spot1, spot2);
+                        gameSessions.Add(gamesesh);
+                        
+                        //spot 3 try to join
+                    }
+                }
                 
-                //string strValue = (string)snapshot.Value; // here is the value of Gamesession
-                //bool f = System.Convert.ToBoolean(strValue);
-                
-                //if(f == false) // ae there is no value at GAMESESSION or the value of gamesession is false
-                //{
-                //    //create a game
-                //}
-                //else
-                //{
-                //    //join that game and close the opening
-                //}
+
+                for (int i = 0; i < gameSessions.Count; i++)
+                {
+                    
+                    if (gameSessions[i].spot1 == "none")
+                    {
+                        Debug.Log("Joining " + gameSessions[i].path + " as player 1");
+                        JoinGame(gameSessions[i].path,"Player1");//put function for joinin a game here
+                        myPath = path;
+                        ListenForPlayerJoin(gameSessions[i].path);
+                        return;
+                    }
+                    else if(gameSessions[i].spot2 == "none")
+                    {
+                        Debug.Log("Joining " + gameSessions[i].path + " as player 2");
+                        JoinGame(gameSessions[i].path, "Player2"); 
+                        return;
+                    }
+                }
             }
         });
     }
+    private void JoinGame(string path, string spot)
+    {
+        var userId = FirebaseAuth.DefaultInstance.CurrentUser.UserId;
+        ProjectileInfo pi = new(0, 0);
+        string js = JsonUtility.ToJson(pi);
+       
+        db.GetReference(GAMELOBBY).Child(GAMESESSION).Child(path).Child(spot).SetValueAsync("Taken").ContinueWithOnMainThread(task => 
+        { 
+            if(task.Exception != null)
+            {
+                Debug.LogWarning(task.Exception);
+            }
+            else
+            {
+                
+            }
+        });
+       
+    }
     private void CreateGame()
     {
-        db?.RootReference.Child(GAMELOBBY).Push().Child(GAMESESSION).SetValueAsync("true").ContinueWithOnMainThread(task =>
+        Game dude = new ();
+        
+        string jsom = JsonUtility.ToJson(dude);
+
+        db?.GetReference(GAMELOBBY).Child(GAMESESSION).Push().SetRawJsonValueAsync(jsom).ContinueWithOnMainThread(task =>
         {
             if (task.Exception != null)
             {
@@ -160,6 +265,7 @@ public class FirebaseTest : MonoBehaviour
             }
         });
     }
+    
     private void MySnap(string jsonString)
     {
         ProjectileInfo obj = JsonUtility.FromJson<ProjectileInfo>(jsonString);
@@ -179,6 +285,27 @@ public class FirebaseTest : MonoBehaviour
         }
 
         Debug.Log("Value has changed: " + args.Snapshot.GetRawJsonValue());
+    }
+    private void ListenForPlayerJoin(string path)
+    {
+       
+        db.GetReference(GAMELOBBY).Child(GAMESESSION).Child(path).Child("Player2").ValueChanged += StartGame;
+       
+    }
+    private void StartGame(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+        Debug.Log(args.Snapshot.GetRawJsonValue());
+        if(args.Snapshot.GetRawJsonValue() == "none")
+        {
+            Debug.Log("Game Will Start " + args.Snapshot.GetRawJsonValue());
+            db.GetReference(GAMELOBBY).Child(GAMESESSION).Child(myPath).Child("Player2").ValueChanged -= StartGame;
+        }
+           
     }
 }
 
